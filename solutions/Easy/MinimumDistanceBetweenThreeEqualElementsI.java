aefe78fc9c6d9bb127602c92a37fec865111ea21
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-04-10 16:28:28 +0530

    feat: solve #4115 Minimum Distance Between Three Equal Elements I [Easy]

--- solutions/Easy/MinimumDistanceBetweenThreeEqualElementsI.java
+++ solutions/Easy/MinimumDistanceBetweenThreeEqualElementsI.java
@@ -8,18 +8,17 @@
 
 class Solution {
     public int minimumDistance(int[] nums) {
-        int res = Integer.MAX_VALUE;
-
-        for(int i=0;i<nums.length-2;i++){
-            for(int j=i+1;j<nums.length-1;j++){
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
                 if(nums[i] == nums[j]){
                     for(int k=j+1;k<nums.length;k++){
                         if(nums[j] == nums[k])
-                            res = Math.min(res, Math.abs(i-j) + Math.abs(j-k) + Math.abs(i-k));
+                            min = Math.min(min, Math.abs(i-j) + Math.abs(j-k) + Math.abs(k-i));
                     }
                 }
             }
         }
-        return res == Integer.MAX_VALUE ? -1:res;
+        return min == Integer.MAX_VALUE ? -1:min;
     }
 }
