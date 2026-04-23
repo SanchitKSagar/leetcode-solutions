2bf08f2b22d486fac82a8dabb121940363e3185e
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-04-23 22:00:43 +0530

    feat: solve #2721 Sum Of Distances [Medium]

---
+++ solutions/Medium/SumOfDistances.java
@@ -0,0 +1,33 @@
+/**
+ * LeetCode #2721 - Sum of Distances
+ * Difficulty : Medium
+ * Topics     : Array, Hash Table, Prefix Sum
+ * Date       : 2026-04-23
+ * URL        : https://leetcode.com/problems/sum-of-distances/
+ */
+
+class Solution {
+
+    public long[] distance(int[] nums) {
+        int n = nums.length;
+        Map<Integer, List<Integer>> groups = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            groups.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+        long[] res = new long[n];
+        for (List<Integer> group : groups.values()) {
+            long total = 0;
+            for (int idx : group) {
+                total += idx;
+            }
+            long prefixTotal = 0;
+            int sz = group.size();
+            for (int i = 0; i < sz; i++) {
+                int idx = group.get(i);
+                res[idx] = total - prefixTotal * 2 + (long) idx * (2 * i - sz);
+                prefixTotal += idx;
+            }
+        }
+        return res;
+    }
+}
