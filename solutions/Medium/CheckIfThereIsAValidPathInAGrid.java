baa7e3ba3e992e2244a2d44f9a551b6bc8306b65
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-04-27 21:27:52 +0530

    feat: solve #1507 Check If There Is A Valid Path In A Grid [Medium]

--- solutions/Medium/CheckIfThereIsAValidPathInAGrid.java
+++ solutions/Medium/CheckIfThereIsAValidPathInAGrid.java
@@ -7,110 +7,40 @@
  */
 
 class Solution {
-
-    class DisjointSet {
-
-        int[] f;
-
-        public DisjointSet(int m, int n) {
-            f = new int[m * n];
-            for (int i = 0; i < m * n; ++i) {
-                f[i] = i;
-            }
-        }
-
-        public int find(int x) {
-            return x == f[x] ? x : (f[x] = find(f[x]));
-        }
-
-        public void merge(int x, int y) {
-            f[find(x)] = find(y);
-        }
-    }
-
-    int[][] grid;
-    int m;
-    int n;
-    DisjointSet ds;
+    private static final int[][] TRANS = {
+        {-1, 1, -1, 3},
+        {0, -1, 2, -1},
+        {3, 2, -1, -1},
+        {1, -1, -1, 2},
+        {-1, 0, 3, -1},
+        {-1, -1, 1, 0}
+    };
+    private static final int[][] DIRS = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    private static final int[][] START = {{1, 3}, {0, 2}, {2, 3},
+                                          {1, 2}, {0, 3}, {0, 1}};
 
     public boolean hasValidPath(int[][] grid) {
-        this.grid = grid;
-        m = grid.length;
-        n = grid[0].length;
-        ds = new DisjointSet(m, n);
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                handler(i, j);
-            }
-        }
-        return ds.find(getId(0, 0)) == ds.find(getId(m - 1, n - 1));
+        int m = grid.length, n = grid[0].length;
+        if (m == 1 && n == 1) return true;
+
+        int[] s = START[grid[0][0] - 1];
+        return check(grid, s[0]) || check(grid, s[1]);
     }
 
-    public int getId(int x, int y) {
-        return x * n + y;
-    }
+    private boolean check(int[][] grid, int di) {
+        if (di == -1) return false;
+        int m = grid.length, n = grid[0].length;
+        int r = DIRS[di][0];
+        int c = DIRS[di][1];
 
-    public void detectL(int x, int y) {
-        if (
-            y - 1 >= 0 &&
-            (grid[x][y - 1] == 4 || grid[x][y - 1] == 6 || grid[x][y - 1] == 1)
-        ) {
-            ds.merge(getId(x, y), getId(x, y - 1));
-        }
-    }
+        while (r >= 0 && r < m && c >= 0 && c < n) {
+            di = TRANS[grid[r][c] - 1][di];
+            if (di == -1 || r == 0 && c == 0) return false;
+            if (r == m - 1 && c == n - 1) return true;
 
-    public void detectR(int x, int y) {
-        if (
-            y + 1 < n &&
-            (grid[x][y + 1] == 3 || grid[x][y + 1] == 5 || grid[x][y + 1] == 1)
-        ) {
-            ds.merge(getId(x, y), getId(x, y + 1));
+            r += DIRS[di][0];
+            c += DIRS[di][1];
         }
-    }
-
-    public void detectU(int x, int y) {
-        if (
-            x - 1 >= 0 &&
-            (grid[x - 1][y] == 3 || grid[x - 1][y] == 4 || grid[x - 1][y] == 2)
-        ) {
-            ds.merge(getId(x, y), getId(x - 1, y));
-        }
-    }
-
-    public void detectD(int x, int y) {
-        if (
-            x + 1 < m &&
-            (grid[x + 1][y] == 5 || grid[x + 1][y] == 6 || grid[x + 1][y] == 2)
-        ) {
-            ds.merge(getId(x, y), getId(x + 1, y));
-        }
-    }
-
-    public void handler(int x, int y) {
-        switch (grid[x][y]) {
-            case 1:
-                detectL(x, y);
-                detectR(x, y);
-                break;
-            case 2:
-                detectU(x, y);
-                detectD(x, y);
-                break;
-            case 3:
-                detectL(x, y);
-                detectD(x, y);
-                break;
-            case 4:
-                detectR(x, y);
-                detectD(x, y);
-                break;
-            case 5:
-                detectL(x, y);
-                detectU(x, y);
-                break;
-            case 6:
-                detectR(x, y);
-                detectU(x, y);
-        }
+        return false;
     }
 }
