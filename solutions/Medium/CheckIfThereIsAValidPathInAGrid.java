3415bb4cef6957018e6ce283d5b1278ad72e4021
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-04-28 08:25:47 +0530

    feat: solve #1507 Check If There Is A Valid Path In A Grid [Medium]

--- solutions/Medium/CheckIfThereIsAValidPathInAGrid.java
+++ solutions/Medium/CheckIfThereIsAValidPathInAGrid.java
@@ -7,39 +7,71 @@
  */
 
 class Solution {
-    private static final int[][] TRANS = {
-        {-1, 1, -1, 3},
-        {0, -1, 2, -1},
-        {3, 2, -1, -1},
-        {1, -1, -1, 2},
-        {-1, 0, 3, -1},
-        {-1, -1, 1, 0}
-    };
-    private static final int[][] DIRS = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    private static final int[][] START = {{1, 3}, {0, 2}, {2, 3},
-                                          {1, 2}, {0, 3}, {0, 1}};
-
     public boolean hasValidPath(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        if (m == 1 && n == 1) return true;
-
-        int[] s = START[grid[0][0] - 1];
-        return check(grid, s[0]) || check(grid, s[1]);
+        Set<String> set = new HashSet<>();
+        switch(grid[0][0]){
+            case 1: return dfs(grid, 0,0, set, 'R');
+            case 2: return dfs(grid, 0,0, set, 'D');
+            case 3: return dfs(grid, 0,0, set, 'D');
+            case 4: return dfs(grid, 0,0, set, 'R') || dfs(grid, 0,0, new HashSet<String>(), 'D');
+            case 5: return (grid.length == 1 && grid[0].length == 1);
+            case 6: return dfs(grid, 0,0, set, 'R');
+        }
+        return false;
     }
 
-    private boolean check(int[][] grid, int di) {
-        if (di == -1) return false;
-        int m = grid.length, n = grid[0].length;
-        int r = DIRS[di][0];
-        int c = DIRS[di][1];
+    public boolean dfs(int[][] grid, int row, int col, Set<String> visited, char dir){
+        if(row<0 || row >= grid.length || col <0 || col >= grid[0].length)
+            return false;
+        
+        if(row == grid.length-1  && col == grid[0].length-1){
+            switch (grid[row][col]){
+                case 1: return dir == 'R';
+                case 2: return dir == 'D';
+                case 3: return dir == 'R';
+                case 4: return false;
+                case 5: return (dir == 'D' || dir == 'R');
+                case 6: return dir == 'D';
+            }
+        }
 
-        while (r >= 0 && r < m && c >= 0 && c < n) {
-            di = TRANS[grid[r][c] - 1][di];
-            if (di == -1 || r == 0 && c == 0) return false;
-            if (r == m - 1 && c == n - 1) return true;
+        String curr = "" + row + "," + col;
+        if(visited.contains(curr))
+            return false;
+        visited.add(curr);
 
-            r += DIRS[di][0];
-            c += DIRS[di][1];
+        switch(grid[row][col]) {
+
+            case 1: 
+            if(curr.equals("0,0") || dir == 'L' || dir == 'R'){
+                return dfs(grid, row, col-1, visited, 'L') || dfs(grid, row, col+1, visited, 'R');
+            }
+            break;
+            case 2:
+            if(curr.equals("0,0") || dir == 'U' || dir == 'D'){
+                return dfs(grid, row-1, col, visited, 'U') || dfs(grid, row+1, col, visited, 'D');
+            }
+            break;
+            case 3:
+            if(curr.equals("0,0") || dir == 'R' || dir == 'U'){
+                return dfs(grid, row, col-1, visited, 'L') || dfs(grid, row+1, col, visited, 'D');
+            }
+            break;
+            case 4:
+            if(curr.equals("0,0") || dir == 'L' || dir == 'U'){
+                return dfs(grid, row, col+1, visited, 'R') || dfs(grid, row+1, col, visited, 'D');
+            }
+            break;
+            case 5:
+            if(curr.equals("0,0") || dir == 'R' || dir == 'D'){
+                return dfs(grid, row-1, col, visited, 'U') || dfs(grid, row, col-1, visited, 'L');
+            }
+            break;
+            case 6:
+            if(curr.equals("0,0") || dir == 'D' || dir == 'L'){
+                return dfs(grid, row-1, col, visited, 'U') || dfs(grid, row, col+1, visited, 'R');
+            }
+            break;
         }
         return false;
     }
