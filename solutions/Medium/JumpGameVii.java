21f5abb4dd7481abc387d0ee718c6fd0ac47ab4b
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-05-25 18:46:32 +0530

    feat: solve #2001 Jump Game Vii [Medium]

---
+++ solutions/Medium/JumpGameVii.java
@@ -0,0 +1,31 @@
+/**
+ * LeetCode #2001 - Jump Game VII
+ * Difficulty : Medium
+ * Topics     : String, Dynamic Programming, Sliding Window, Prefix Sum
+ * Date       : 2026-05-25
+ * URL        : https://leetcode.com/problems/jump-game-vii/
+ */
+
+class Solution {
+
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        int[] f = new int[n];
+        int[] pre = new int[n];
+        f[0] = 1;
+        // since we start dynamic programming from i=minJump, we need to precompute the prefix sums for the part [0, minJump)
+        for (int i = 0; i < minJump; i++) {
+            pre[i] = 1;
+        }
+        for (int i = minJump; i < n; i++) {
+            int left = i - maxJump;
+            int right = i - minJump;
+            if (s.charAt(i) == '0') {
+                int total = pre[right] - (left <= 0 ? 0 : pre[left - 1]);
+                f[i] = total != 0 ? 1 : 0;
+            }
+            pre[i] = pre[i - 1] + f[i];
+        }
+        return f[n - 1] == 1;
+    }
+}
