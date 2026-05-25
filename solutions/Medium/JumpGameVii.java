a7051da6a00c31100f6e5aa9fdbf10a3a5a47cff
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-05-25 19:32:55 +0530

    feat: solve #2001 Jump Game Vii [Medium]

--- solutions/Medium/JumpGameVii.java
+++ solutions/Medium/JumpGameVii.java
@@ -7,25 +7,22 @@
  */
 
 class Solution {
-
     public boolean canReach(String s, int minJump, int maxJump) {
-        int n = s.length();
-        int[] f = new int[n];
-        int[] pre = new int[n];
-        f[0] = 1;
-        // since we start dynamic programming from i=minJump, we need to precompute the prefix sums for the part [0, minJump)
-        for (int i = 0; i < minJump; i++) {
-            pre[i] = 1;
-        }
-        for (int i = minJump; i < n; i++) {
-            int left = i - maxJump;
-            int right = i - minJump;
-            if (s.charAt(i) == '0') {
-                int total = pre[right] - (left <= 0 ? 0 : pre[left - 1]);
-                f[i] = total != 0 ? 1 : 0;
+        boolean[] b = new boolean[s.length()];
+        b[0] = true;
+        int left = 0, right = 0;
+        for(int i=0;i<s.length();i++){
+            if(!b[i])
+                continue;
+            left = Math.max(right+1, i+minJump);
+            right = Math.min( s.length()-1,i+maxJump);
+            for(int j=left;j<=right;j++){
+                if(s.charAt(j) == '0')
+                    b[j] = true;
             }
-            pre[i] = pre[i - 1] + f[i];
+            if(b[s.length()-1])
+                return true;
         }
-        return f[n - 1] == 1;
+        return false;
     }
 }
