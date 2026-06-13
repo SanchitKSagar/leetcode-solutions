3845132c67d81cb0f15eba082fc28ac03a691b7f
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-06-13 08:44:05 +0530

    feat: solve #4216 Weighted Word Mapping [Easy]

--- solutions/Easy/WeightedWordMapping.java
+++ solutions/Easy/WeightedWordMapping.java
@@ -8,19 +8,13 @@
 
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
-        Map<Integer, Character> map = new HashMap<>();
         String res = "";
-        for(int i=0;i<26;i++){
-            map.put(i, (char)('z' - i));
-        }    
-
         int sum = 0;
         for(String word:words){
             sum = 0;
             for(char ch : word.toCharArray())
                 sum += weights[(int)(ch-'a')];
-            //System.out.println(map + " " + sum + " " + sum%26 + " " + map.get(sum % 26));
-            res += map.get(sum % 26);
+            res += (char)('z' - (sum % 26));
         }
 
         return res;
