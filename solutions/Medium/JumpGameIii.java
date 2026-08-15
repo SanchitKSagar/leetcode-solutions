306e553bc4f9d3cd70f3e4d5c07b8645dda84d43
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-08-15 15:36:34 +0530

    feat: solve #1428 Jump Game Iii [Medium]

--- solutions/Medium/JumpGameIii.java
+++ solutions/Medium/JumpGameIii.java
@@ -2,23 +2,26 @@
  * LeetCode #1428 - Jump Game III
  * Difficulty : Medium
  * Topics     : Array, Depth-First Search, Breadth-First Search
- * Date       : 2026-05-17
+ * Date       : 2026-08-15
  * URL        : https://leetcode.com/problems/jump-game-iii/
  */
 
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        return canReach(arr, start, new HashSet<Integer>());
+        return canReach(arr, start, new boolean[arr.length+1]);
     }
-    public boolean canReach(int[] arr, int idx, Set<Integer> visited){
-        if(idx <0 || idx >= arr.length)
-            return false;
-        if(arr[idx] == 0)
-            return true;
-        if(visited.contains(idx))
-            return false;
 
-        visited.add(idx);
+    public boolean canReach(int[] arr, int idx, boolean[] visited){
+        if(idx < 0 || idx >= arr.length)
+            return false;
+        if(visited[idx])
+            return false;
+        if(arr[idx] == 0){
+            visited[idx] = true;
+            return true;
+        }
+
+        visited[idx] = true;
         return canReach(arr, idx + arr[idx], visited) || canReach(arr, idx - arr[idx], visited);
     }
 }
