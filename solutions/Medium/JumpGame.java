5ea45e9a978279dffe1367caaab7158b4a681f03
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-08-15 15:13:03 +0530

    feat: solve #55 Jump Game [Medium]

--- solutions/Medium/JumpGame.java
+++ solutions/Medium/JumpGame.java
@@ -17,27 +17,40 @@
 //     }
 // }
 
-class Solution {
-    Boolean[] dp;
-    public boolean canJump(int[] nums) {
-        dp = new Boolean[nums.length+1];
-        return canJump(nums, 0);
-    }
-    public boolean canJump(int[] nums, int idx){
-        if(idx == nums.length-1){
-            dp[idx] = true;
-            return true;
-        }
-        if(idx >= nums.length)
-            return false;
-        if(dp[idx] != null)
-            return dp[idx];
+// class Solution {
+//     Boolean[] dp;
+//     public boolean canJump(int[] nums) {
+//         dp = new Boolean[nums.length+1];
+//         return canJump(nums, 0);
+//     }
+//     public boolean canJump(int[] nums, int idx){
+//         if(idx == nums.length-1){
+//             dp[idx] = true;
+//             return true;
+//         }
+//         if(idx >= nums.length)
+//             return false;
+//         if(dp[idx] != null)
+//             return dp[idx];
 
-        boolean b = false;
-        for(int i=1;i<=nums[idx] && !b;i++){
-            b |= canJump(nums, idx+i);
+//         boolean b = false;
+//         for(int i=1;i<=nums[idx] && !b;i++){
+//             b |= canJump(nums, idx+i);
+//         }
+//         dp[idx] = b;
+//         return b;
+//     }
+// }
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i] + i >= goal){
+                goal = i;
+            }
         }
-        dp[idx] = b;
-        return b;
+        return goal == 0;
     }
 }
+
