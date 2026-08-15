3606f20b4d93da4dde59520333ad15a1302e1d53
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-08-15 09:28:37 +0530

    feat: solve #383 Ransom Note [Easy]

---
+++ solutions/Easy/RansomNote.java
@@ -0,0 +1,28 @@
+/**
+ * LeetCode #383 - Ransom Note
+ * Difficulty : Easy
+ * Topics     : Hash Table, String, Counting
+ * Date       : 2026-08-15
+ * URL        : https://leetcode.com/problems/ransom-note/
+ */
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] magArr = new int[26];
+        int[] ransomArr = new int[26];
+        for(char c:magazine.toCharArray()){
+            magArr[c-'a']++;
+        }
+
+        for(char c:ransomNote.toCharArray()){
+            ransomArr[c-'a']++;
+        }
+
+        for(int i=0;i<26;i++){
+            if(ransomArr[i] != 0 && ransomArr[i] > magArr[i])
+                return false;
+        }
+        return true;
+
+    }
+}
