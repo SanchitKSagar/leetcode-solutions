0dfe573444ed8e215fff80f68a7f6e4fea3b0f1e
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-09-08 00:40:12 +0530

    feat: solve #977 Distinct Subsequences Ii [Hard]

---
+++ solutions/Hard/DistinctSubsequencesIi.java
@@ -0,0 +1,26 @@
+/**
+ * LeetCode #977 - Distinct Subsequences II
+ * Difficulty : Hard
+ * Topics     : String, Dynamic Programming
+ * Date       : 2026-09-07
+ * URL        : https://leetcode.com/problems/distinct-subsequences-ii/
+ */
+
+class Solution {
+    int MOD = 1000000007;
+
+    public int distinctSubseqII(String s) {
+        int tot = 0;
+        int[] dp = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i) - 97;
+            int add = (tot - dp[c] + MOD) % MOD;
+
+            dp[c] = 1 + tot;
+            tot = (dp[c] + add) % MOD;
+        }
+
+        return tot;
+    }
+}
