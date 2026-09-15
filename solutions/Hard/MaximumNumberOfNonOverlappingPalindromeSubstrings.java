98e74699ac35959d767b83e958019b264e8f29f7
SanchitKSagar <sagar.sanchit.zen.1998@gmail.com>
2026-09-15 23:41:01 +0530

    feat: solve #2559 Maximum Number Of Non Overlapping Palindrome Substrings [Hard]

---
+++ solutions/Hard/MaximumNumberOfNonOverlappingPalindromeSubstrings.java
@@ -0,0 +1,36 @@
+/**
+ * LeetCode #2559 - Maximum Number of Non-overlapping Palindrome Substrings
+ * Difficulty : Hard
+ * Topics     : Two Pointers, String, Dynamic Programming, Greedy
+ * Date       : 2026-09-15
+ * URL        : https://leetcode.com/problems/maximum-number-of-non-overlapping-palindrome-substrings/
+ */
+
+class Solution {
+
+    public int maxPalindromes(String s, int k) {
+        int n = s.length();
+        boolean[][] isPalindrome = new boolean[n][n];
+
+        for (int len = 1; len <= n; ++len) {
+            for (int left = 0; left + len <= n; ++left) {
+                int right = left + len - 1;
+                isPalindrome[left][right] =
+                    s.charAt(left) == s.charAt(right) &&
+                    (len <= 2 || isPalindrome[left + 1][right - 1]);
+            }
+        }
+
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = dp[i - 1];
+            for (int j = 0; j + k <= i; ++j) {
+                if (isPalindrome[j][i - 1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
